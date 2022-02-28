33d8b9d7960d725ce673b695c16c99389dd5b305
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-28 12:14:29 +0530

    Added: withwal ammout remaning amout displayed

--- src/stock/java/StockAccount.java
+++ src/stock/java/StockAccount.java
@@ -1,88 +1,31 @@
 package stock.java;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StockAccount {
 	static Scanner scanner = new Scanner(System.in);
+	private double balance;
 
-	ArrayList<StockManagement> arrayList = new ArrayList<>();
-	static double balance = 10000000;
+	public StockAccount(double balance) {
+		super();
+		this.balance = balance;
+	}
 
-	public void addStock() {
-		StockManagement StockManagement;
-		StockManagement = new StockManagement();
-		double withdraw = 0;
-
-		System.out.println(" Enter the stock name");
-		scanner.nextLine();
-		StockManagement.setStockNames(scanner.nextLine());
-
-		System.out.println("Enter the Number of shares");
-		StockManagement.setNumOfShare(scanner.nextInt());
-
-		System.out.println("Enter the each share price");
-		StockManagement.setSharePrice(scanner.nextDouble());
-		withdraw = StockManagement.getSharePrice() * StockManagement.getNumOfShare();
-		if (balance >= withdraw) {
+	public void debit(double balance) {
+		System.out.println("enter ammount to withdraw");
+		double withdraw = scanner.nextDouble();
+		if (withdraw > balance)
+			System.out.println("withdraw exceeds the balance please enter correct ammount");
+		else
 			balance = balance - withdraw;
-			arrayList.add(StockManagement);
-			System.out.println("Remaining balance = " + balance);
-		} else
-
-			debit(withdraw);
-	}
-
-	public void printStockReport() {
-		StockManagement StockManagement;
-		StockManagement = new StockManagement();
-		for (int i = 0; i < arrayList.size(); i++) {
-			// StockManagement = arrayList.get(i);
-			// double totalPrice = s.getNumOfShare() * s.getSharePrice();
-			System.out.println("............. **STOCK REPORT**..............");
-			System.out.println(StockManagement.toString());
-			// System.out.println("stock name = " + s.getStockNames() + "\n number of shares
-			// = " + s.getNumOfShare()
-			// + "\n stock price = " + s.getSharePrice() + "\nTotal price of stock = " +
-			// totalPrice + "\n");
-
-		}
-	}
-
-	public void debit(double withdraw) {
-		if (withdraw > balance) {
-			System.out.println("Debit amount exceeded account balance.");
-			System.out.println("Remaining balance = " + balance);
-			System.out.println("withdraw = " + withdraw);
-		}
+		System.out.println("Remaining balance is " + balance);
 
 	}
 
-	/*
-	 * public void stockReport() { Double sum = 0.0;
-	 * System.out.println("............. **STOCK REPORT**.............."); //
-	 * System.out.println("stock name Number of shares Stock price Total price of //
-	 * stock "); for (int i = 0; i < arrayList.size(); i++) { StockManagement s =
-	 * arrayList.get(i); double totalPrice = s.getNumOfShare() * s.getSharePrice();
-	 * totalPrice = sum + totalPrice; System.out.println("stock name = " +
-	 * s.getStockNames() + "\n number of shares = " + s.getNumOfShare() +
-	 * "\n stock price = " + s.getSharePrice() + "\nTotal price of stock = " +
-	 * totalPrice + "\n"); // System.out.format("%-10s %-10.3f %-10.3f %-10.3f\n",
-	 * s.getStockNames(), // s.getNumOfShare(), s.getSharePrice(), totalPrice); } }
-	 */
 	public static void main(String[] args) {
-		StockAccount s = new StockAccount();
-		while (true) {
-			System.out.println("Enter the selection\n1)addStock\n2)displayStockReport\n3)exit");
-			byte choice = scanner.nextByte();
-			switch (choice) {
-			case 1 -> s.addStock();
-			case 2 -> s.printStockReport();
-			// case 3 -> stockManagement.printStockReport();
-			case 3 -> System.exit(1);
-			}
-		}
-
+		System.out.println("Enter the balance to mention in the account");
+		StockAccount account = new StockAccount(scanner.nextDouble());
+		account.debit(account.balance);
 	}
 
 }
